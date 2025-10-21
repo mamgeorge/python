93281ec5ad44707f0d7819b0308016856b68e72c
mamgeorge <mamgeorge@yahoo.com>
2025-10-20 20:28:43 -0400

    added com file

---
+++ sample_gcp/xtra/com.sh
@@ -0,0 +1,17 @@
+
+com.sh
+#!/bin/bash
+
+clear
+echo "▓▒░ COMMANDS ░▒▓█"
+whoami
+pwd
+# printenv | sort | nl
+# env | sort | grep -v LS_COLORS
+date -Is
+hostname -I
+ls -plaX
+df -h
+free -m
+lsb_release -c
+	
